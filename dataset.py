b5f743a3fc8032493a8bf1035177d8bfa6fbb52c
Fabian <fabian.schramm@tum.de>
2020-01-23 15:21:32 +0900

    default dataset class

--- dataset.py
+++ dataset.py
@@ -6,9 +6,8 @@ import chainercv.transforms as transforms
 import numpy as np
 
 from augment import random_rotate, random_flip, random_crop
-from augment import scale_fit_short, resize, resize_to_scale
+from augment import scale_fit_short, resize
 from augment import augment_image
-from utils import show_image1
 
 
 class KeypointDataset2D(DatasetMixin):
@@ -23,8 +22,6 @@ class KeypointDataset2D(DatasetMixin):
                  bbox,
                  is_visible,
                  is_labeled,
-                 scale,
-                 position,
                  image_paths,
                  image_root='.',
                  use_cache=False,
@@ -39,8 +36,6 @@ class KeypointDataset2D(DatasetMixin):
         self.bbox = bbox  # [x,y,w,h]
         self.is_visible = is_visible
         self.is_labeled = is_labeled
-        self.scale = scale
-        self.position = position
         self.image_paths = image_paths
         self.image_root = image_root
         self.do_augmentation = do_augmentation
@@ -50,13 +45,12 @@ class KeypointDataset2D(DatasetMixin):
     def __len__(self):
         return len(self.image_paths)
 
-    def transform(self, image, keypoints, bbox, is_labeled, is_visible, dataset_type, scale):
+    def transform(self, image, keypoints, bbox, is_labeled, is_visible, dataset_type):
         transform_param = {}
 
         # Color augmentation
-        # image, param = augment_image(image, dataset_type)
-        # transform_param['augment_image'] = param
-        transform_param['augment_image'] = "0"
+        image, param = augment_image(image, dataset_type)
+        transform_param['augment_image'] = param
 
         # Random rotate
         image, keypoints, bbox, param = random_rotate(image, keypoints, bbox)
@@ -67,12 +61,9 @@ class KeypointDataset2D(DatasetMixin):
         transform_param['random_flip'] = param
 
         # Random crop
-        # image, keypoints, bbox, param = random_crop(image, keypoints, bbox, is_labeled, dataset_type)
-        # transform_param['random_crop'] = param
-        transform_param['random_crop'] = "0"
+        image, keypoints, bbox, param = random_crop(image, keypoints, bbox, is_labeled, dataset_type)
+        transform_param['random_crop'] = param
 
-        # scale to approx 200 px
-        # image, keypoints, bbox = resize_to_scale(image, keypoints, bbox, scale=scale, insize=self.insize)
         return image, keypoints, bbox, is_labeled, is_visible, transform_param
 
     def get_example(self, i):
@@ -87,8 +78,6 @@ class KeypointDataset2D(DatasetMixin):
             bbox = self.bbox[i]
             is_labeled = self.is_labeled[i]
             is_visible = self.is_visible[i]
-            scale = self.scale[i]
-            position = self.position[i]
 
             if self.use_cache:
                 image, keypoints, bbox = resize(image, keypoints, bbox, (h, w))
@@ -99,32 +88,15 @@ class KeypointDataset2D(DatasetMixin):
         bbox = bbox.copy()
         is_labeled = is_labeled.copy()
         is_visible = is_visible.copy()
-        scale = scale.copy()
-        # position = position.copy()
 
         transform_param = {}
         try:
-            # put it here for testing with provided MPII dataset - otherwise scale of persons is
-            # to different and can't detect limbs with grid 3x3 -> huge error for limbs
-            # usually should not be necessary when testing with proper video dataset
-            image, keypoints, bbox = resize_to_scale(image, keypoints, bbox, scale=scale, insize=self.insize)
-
             if self.do_augmentation:
-                # image, keypoints, bbox = scale_fit_short(image, keypoints, bbox, length=int(min(h, w) * 1.25))
-                # TODO activate this line here and uncomment above resize_to_scale when testing with real data
-                # image, keypoints, bbox = resize_to_scale(image, keypoints, bbox, scale=scale, insize=self.insize)
-                # utils.write_image(image, os.path.join('/home/fabian/Desktop/test/', self.image_paths[i]))
-
-                # adapted transforms, random crop destroys scaling to normalized
+                image, keypoints, bbox = scale_fit_short(image, keypoints, bbox, length=int(min(h, w) * 1.25))
                 image, keypoints, bbox, is_labeled, is_visible, transform_param = self.transform(
-                    image, keypoints, bbox, is_labeled, is_visible, self.dataset_type, scale)
-                # utils.write_image(image, os.path.join('/home/fabian/Desktop/test4/', self.image_paths[i]))
-
-                # should be one intend level up when tested with real dataset and resize_to_scale is deactivated
-                image, keypoints, bbox = resize(image, keypoints, bbox, (h, w))
-
+                    image, keypoints, bbox, is_labeled, is_visible, self.dataset_type)
             transform_param['do_augmentation'] = self.do_augmentation
-
+            image, keypoints, bbox = resize(image, keypoints, bbox, (h, w))
         except Exception as e:
             raise Exception("something wrong...transform_param = {}".format(transform_param))
 
