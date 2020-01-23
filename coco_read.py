7546e2e4961bbf6acbfba23d21c5fe8d37e91ebb
Fabian <fabian.schramm@tum.de>
2020-01-23 11:26:24 +0900

    crop images according to person - not square

--- coco_read.py
+++ coco_read.py
@@ -7,7 +7,8 @@ import matplotlib.pyplot as plt
 import pylab
 
 MIN_KEYPOINTS = 10
-MIN_HEIGHT = 100
+MIN_HEIGHT = 150
+SSIZE = 384  # size of cropped square
 
 pylab.rcParams['figure.figsize'] = (8.0, 10.0)
 
@@ -23,24 +24,15 @@ coco=COCO(annFile)
 annFile = '{}/annotations/person_keypoints_{}.json'.format(dataDir, dataType)
 coco_kps=COCO(annFile)
 
-# display COCO categories and supercategories
-cats = coco.loadCats(coco.getCatIds())
-# nms=[cat['name'] for cat in cats]
-# print('COCO categories: \n{}\n'.format(' '.join(nms)))
-
-# nms = set([cat['supercategory'] for cat in cats])
-# print('COCO supercategories: \n{}'.format(' '.join(nms)))
-
 # get all images containing given person instance (only them labeled with keypoints)
 catIds = coco.getCatIds(catNms=['person'])  # 1
 imgIds = coco.getImgIds(catIds=catIds )  # for train2017 len(imgIds) = 64,115 , val2017 len(imgIds) = 2693
-#imgIds = coco.getImgIds(imgIds = [324158])
 # random select
 # img = coco.loadImgs(imgIds[np.random.randint(0,len(imgIds))])[0]
 
 s = 0  # TODO debug variable to break for loop
 for imgId in imgIds:
-    if s > 3:
+    if s > 300:
         break
     s = s + 1
     # load image
@@ -63,21 +55,28 @@ for imgId in imgIds:
             y = int(y)
             w = int(w)
             h = int(h)
-            # crop_img = I[y:y + h, x:x + w]
-            # plt.imshow(crop_img)
-            # plt.axis('off')
-            # plt.savefig('cropp{}.png'.format(img['file_name']))
 
-            crop_img2 = I2[y:y + h, x:x + w]
+            # handle failure cases of slicing
+            if x < 0:
+                x = 0
+            if y < 0:
+                y = 0
+            if x + w > img['width']:
+                w = img['width']
+            else:
+                w = x + w
+            if y + h > img['height']:
+                h = img['height']
+            else:
+                h = y + h
+
+            crop_img2 = I2[y:h, x:w]
             # cv2.imshow("cropped", crop_img2)
             # cv2.waitKey(0)
-            im_path = '{}/images_cropped/{}/{}_{}.jpg'.format(dataDir, dataType,img_name, ann['id'])
+            im_path = '{}/images_cropped/{}/{}_annid{}.jpg'.format(dataDir, dataType, img_name, ann['id'])
             cv2.imwrite(im_path, crop_img2)
 
-
-
-
-
+    print('{} done so far'.format(s))
 
     # save picture with annotations
     # plt.imshow(I); plt.axis('off')
